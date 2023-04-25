44ca26bdb806287b83bbf7f9cf2fe96e61d66856
yuki <c0a2210212@edu.teu.ac.jp>
2023-04-25 14:11:56 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,8 +8,10 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img= pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
     tmr = 0
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -18,6 +20,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img, [600, 200])
 
         pg.display.update()
         clock.tick(1000)
