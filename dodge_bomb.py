0ff672c4d3c29e1f50d235a2b1a8d290ebb44c8d
yuki <c0a2210212@edu.teu.ac.jp>
2023-04-25 16:32:57 +0900

    追加機能２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,8 +8,14 @@ delta = {
         pg.K_LEFT: (-1, 0), 
         pg.K_RIGHT: (1, 0), 
         }
-
+accs = [a for a in range(1, 11)]
 def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
+    引数１：画面SurfaceのRect
+    引数２：こうかとん，または，爆弾SurfaceのRect
+    戻り値：横方向，縦方向のはみ出し判定結果（画面内：True／画面外：False）
+    """
     yoko, tate = True, True
     if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
         yoko = False
@@ -23,6 +29,7 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+   
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
 
@@ -44,11 +51,12 @@ def main():
                 return 0
 
         tmr += 1
-
+        avx, avy = vx * accs[min(tmr//1000, 9)], vy*accs[min(tmr//1000,9)]
         key_lst = pg.key.get_pressed() 
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
+        
         if check_bound(screen.get_rect(), kk_rct) != (True, True):
             for k, mv in delta.items():
                 if key_lst[k]:
@@ -56,7 +64,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+        bb_rct.move_ip(avx, avy)
         yoko, tate = check_bound(screen.get_rect(), bb_rct)
         if not yoko:
             vx *= -1
